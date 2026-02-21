7178bc6d3404d9e8e54f79a42954dde76074747e
smallstepman <redacted@example.invalid>
2026-02-21 17:59:20 +0100

    darwin: `defaults` options setup

--- users/m/darwin.nix
+++ users/m/darwin.nix
@@ -54,6 +54,60 @@ in
 
   # Required for some settings like homebrew to know what user to apply to.
   system.primaryUser = "m";
+
+  system.defaults.CustomUserPreferences = {
+    "com.apple.finder" = {
+      AppleShowAllFiles = true;
+      ShowPathbar = true;
+      ShowStatusBar = true;
+      _FXShowPosixPathInTitle = true;
+      FXPreferredViewStyle = "Nlsv";
+      CreateDesktop = false;
+    };
+
+    "com.apple.dock" = {
+      autohide = true;
+      tilesize = 36;
+      magnification = true;
+      largesize = 64;
+      "minimize-to-application" = true;
+      "show-recents" = false;
+      "mru-spaces" = false;
+      "expose-animation-duration" = 0.1;
+      "autohide-delay" = 0.0;
+      "autohide-time-modifier" = 0;
+    };
+
+    NSGlobalDomain = {
+      KeyRepeat = 1;
+      InitialKeyRepeat = 8;
+      ApplePressAndHoldEnabled = false;
+      "com.apple.mouse.scaling" = 0.0;
+      "com.apple.mouse.tapBehavior" = 1;
+      "com.apple.trackpad.scaling" = 10.0;
+      NSWindowShouldDragOnGesture = true;
+      NSAutomaticWindowAnimationsEnabled = false;
+      NSWindowResizeTime = 0.001;
+    };
+
+    "com.apple.screencapture" = {
+      location = "/Users/m/Pictures/Screenshots";
+      type = "png";
+    };
+
+    "com.apple.menuextra.clock" = {
+      DateFormat = "EEE MMM d  H:mm";
+    };
+
+    "com.apple.speech.recognition.AppleSpeechRecognition.prefs" = {
+      DictationIMAllowAudioDucking = false;
+    };
+
+    "com.apple.SpeechRecognitionCore" = {
+      AllowAudioDucking = false;
+    };
+  };
+
   services.skhd = {
     enable = true;
     package = pkgs.skhd;
