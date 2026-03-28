abd4a480131a267c4bc7e87cc8c9d663243e4477
Marcin Nowak Liebiediew <m.liebiediew@gmail.com>
2026-03-28 18:50:12 +0100

    Wire sudo through Touch ID bridge

--- den/aspects/features/linux-core.nix
+++ den/aspects/features/linux-core.nix
@@ -82,6 +82,17 @@
           security.sudo.extraConfig = ''
             Defaults timestamp_timeout=10
           '';
+          security.pam.services.sudo.rules.auth."vm-touchid-bridge" = lib.mkIf
+            config.virtualisation.vmware.guest.enable
+            {
+              order = config.security.pam.services.sudo.rules.auth.unix.order - 50;
+              control = "sufficient";
+              modulePath = "${config.security.pam.package}/lib/security/pam_exec.so";
+              args = [
+                "quiet"
+                "/run/current-system/sw/bin/vm-touchid-sudo-bridge"
+              ];
+            };
 
           # ---------------------------------------------------------------
           # Virtualization
