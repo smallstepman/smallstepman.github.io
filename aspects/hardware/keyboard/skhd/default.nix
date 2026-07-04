766e7e6f4f88ff1214881159b3f4109ea2582296
Marcin Nowak Liebiediew <m.liebiediew@gmail.com>
2026-07-04 18:08:02 +0200

    shkd adjustments and sevice fixes

--- aspects/hardware/keyboard/skhd/default.nix
+++ aspects/hardware/keyboard/skhd/default.nix
@@ -1,15 +1,45 @@
 {
   den.aspects.keyboard.skhd = {
-    darwin = { pkgs, ... }: {
-      services.skhd = {
+    darwin = { lib, pkgs, ... }: let
+      skhdPath = lib.concatStringsSep ":" [
+        "/Users/m/.nix-profile/bin"
+        "/Users/m/.cargo/bin"
+        "/etc/profiles/per-user/m/bin"
+        "/run/current-system/sw/bin"
+        "/nix/var/nix/profiles/default/bin"
+        "/opt/homebrew/bin"
+        "/usr/local/bin"
+        "/usr/bin"
+        "/bin"
+        "/usr/sbin"
+        "/sbin"
+      ];
+    in {
+      services.skhd.enable = lib.mkForce false;
+
+      home-manager.users.m.services.skhd = {
         enable = true;
         package = pkgs.skhd;
-        skhdConfig = builtins.readFile ./skhdrc;
+        config = ./skhdrc;
+        errorLogFile = "/tmp/skhd.err.log";
+        outLogFile = "/tmp/skhd.out.log";
+      };
+
+      home-manager.users.m.launchd.agents.skhd.config.ProgramArguments = lib.mkForce [
+        (lib.getExe pkgs.skhd)
+        "-c"
+        "/Users/m/.config/skhd/skhdrc"
+        "-h"
+      ];
+
+      home-manager.users.m.launchd.agents.skhd.config.EnvironmentVariables = {
+        PATH = skhdPath;
+        SHELL = "/bin/bash";
       };
     };
 
-    homeManager = { pkgs, ... }: {
-      home.packages = [ pkgs.skhd ];
+    homeManager = { lib, pkgs, ... }: {
+      home.packages = lib.optionals pkgs.stdenv.isDarwin [ pkgs.skhd ];
     };
   };
 }
